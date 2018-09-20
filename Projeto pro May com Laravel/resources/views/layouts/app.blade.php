a1ad13935d0215c28cfa2d65a327c06fe19dc5a7
Marcelo Moreira de Oliveira <maxcelo.lima1000@gmail.com>
2018-09-19 21:08:33 -0300

    Adicionando os arquivos do site básico para o laravel

---
+++ Projeto pro May com Laravel/resources/views/layouts/app.blade.php
@@ -0,0 +1,48 @@
+<div>
+    @yield('content')
+</div>
+<!--Inicio Rodapé-->
+<div class="container">
+    <div class="row">
+        <div class="col-12 mb-3">
+            <hr>
+        </div>
+        <div class="col-md-4 ml-5">
+            <h3 class="title">Localização</h3>
+            <div class="embed-responsive embed-responsive-16by9">
+                <iframe class="embed-responsive-item" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d124767.7071291072!2d-38.997715674099524!3d-12.248898366774933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x71437971e302841%3A0xf1ba858abc569a83!2sMixcom+Controle+de+Ponto+e+Acesso!5e0!3m2!1spt-BR!2sbr!4v1535730800761"></iframe>
+            </div>
+        </div>
+        <div class=" col-sm-4 ml-3">
+            <h3>Contatos</h3>
+            <div class="list-group text-center">
+                <p class="text-left">Telefone:(75) 3626.8508</p>
+                <p class="text-left">Alex Lira</p>
+                <p class="text-left">alexlira@mixcom.net.br</p>
+                <p class="text-left">Lívia Andrade</p>
+                <p class="text-left">livia@mixcom.net.br</p>
+            </div>
+        </div>
+
+        <div class="col-sm-3">
+            <h3>Social</h3>
+            <div class="btn-group-vertical btn-block btn-group-lg" role="group">
+                <a class="btn btn-outline-primary" href="https://www.facebook.com/Mixcompontoeacesso/?timeline_context_item_type=intro_card_work&timeline_context_item_source=100003811757067&fref=tag"><i
+                        class="fab fa-facebook"></i> Facebook</a>
+                <a class="btn btn-outline-info" href="#"><i class="fab fa-twitter"></i> Twitter</a>
+                <a class="btn btn-outline-warning" href="#"><i class="fab fa-instagram"></i> Instagram</a>
+            </div>
+        </div>
+
+        <div class="col-12 mt-5">
+            <blockquote class="blockquote text-center">
+                <a href="#" class="blockquote-footer">&copy; 2018 Desenvolvido pela <cite title="Titulo"> Equipe de
+                        PFC
+                        - MIXCOM</cite></a>
+            </blockquote>
+
+        </div>
+
+    </div>
+</div>
+<!--Fim Rodapé-->
